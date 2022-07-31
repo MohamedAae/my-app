4efb5cf9b4f734ea05fdd06d10b02278880581c5
Mohamed Aae <madsaaeq@gmail.com>
2022-07-31 23:48:43 +0200

    Get products from backend

--- src/Home/components/best-seller/best-seller.jsx
+++ src/Home/components/best-seller/best-seller.jsx
@@ -1,101 +1,14 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./best-seller.css";
+import axios from "axios";
+
 export default function BestSeller() {
-  const bestbook = [
-    {
-      id: 1,
-      price: 125,
-      cover:
-        "https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B300%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&product=path%5B/pimages/9780439064873_p0_v1%5D&call=url%5Bfile:common/decodeProduct.chain%5D",
-      bookurl: "",
-      author: "Alison Espach",
-      description:
-        "In this exhilarating novel, two friends—often in love, but never lovers—come together as creative partners in the world of video game design, where success brings them fame, joy, tragedy, duplicity, and, ultimately, a kind of immortality.",
-      title: "The Night Shift: ANovel",
-      rate: 5,
-    },
-    {
-      id: 2,
-      price: 125,
-      cover:
-        "https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B300%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&product=path%5B/pimages/9780439064873_p0_v1%5D&call=url%5Bfile:common/decodeProduct.chain%5D",
-      bookurl: "",
-      author: "Alison Espach",
-      description: "ftttt",
-      title: "The Night Shift: ANovel",
-      rate: 3,
-    },
-    {
-      id: 4,
-      price: 125,
-      cover:
-        "https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B300%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&product=path%5B/pimages/9780439064873_p0_v1%5D&call=url%5Bfile:common/decodeProduct.chain%5D",
-      bookurl: "",
-      author: "Alison Espach",
-      description: "ftttt",
-      title: "The Night Shift: ANovel",
-      rate: 4,
-    },
-    {
-      id: 6,
-      price: 125,
-      cover:
-        "https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B300%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&product=path%5B/pimages/9780439064873_p0_v1%5D&call=url%5Bfile:common/decodeProduct.chain%5D",
-      bookurl: "",
-      author: "Alison Espach",
-      description: "ftttt",
-      title: "The Night Shift: ANovel",
-      rate: 4,
-    },
-    {
-      id: 9,
-      price: 125,
-      cover:
-        "https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B300%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&product=path%5B/pimages/9780439064873_p0_v1%5D&call=url%5Bfile:common/decodeProduct.chain%5D",
-      bookurl: "",
-      author: "Alison Espach",
-      description: "ftttt",
-      title: "The Night Shift: ANovel",
-      rate: 4,
-    },
+  const bestBook = [
     {
       id: 11,
       price: 125,
       cover:
-        "https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B300%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&product=path%5B/pimages/9780439064873_p0_v1%5D&call=url%5Bfile:common/decodeProduct.chain%5D",
-      bookurl: "",
-      author: "Alison Espach",
-      description: "ftttt",
-      title: "The Night Shift: ANovel",
-      rate: 4,
-    },
-    {
-      id: 11,
-      price: 125,
-      cover:
-        "https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B300%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&product=path%5B/pimages/9780439064873_p0_v1%5D&call=url%5Bfile:common/decodeProduct.chain%5D",
-      bookurl: "",
-      author: "Alison Espach",
-      description: "ftttt",
-      title: "The Night Shift: ANovel",
-      rate: 4,
-    },
-    {
-      id: 11,
-      price: 125,
-      cover:
-        "https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B300%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&product=path%5B/pimages/9780439064873_p0_v1%5D&call=url%5Bfile:common/decodeProduct.chain%5D",
-      bookurl: "",
-      author: "Alison Espach",
-      description: "ftttt",
-      title: "The Night Shift: ANovel",
-      rate: 4,
-    },
-    {
-      id: 11,
-      price: 125,
-      cover:
-        "https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B300%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&product=path%5B/pimages/9780439064873_p0_v1%5D&call=url%5Bfile:common/decodeProduct.chain%5D",
+          "https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B300%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&product=path%5B/pimages/9780439064873_p0_v1%5D&call=url%5Bfile:common/decodeProduct.chain%5D",
       bookurl: "",
       author: "Alison Espach",
       description: "ftttt",
@@ -103,6 +16,16 @@ export default function BestSeller() {
       rate: 4,
     },
   ];
+
+  const [bestBooks, setBestBooks] = useState();
+
+  useEffect(() => {
+    axios.get('http://loaclhost:5003/products').then((res) => {
+      const products = res.data;
+      console.log(res.data);
+      setBestBooks(products);
+    }).catch((err) => console.error(err.message));
+  });
 
   function forloop(n) {
     let starsarr = [];
@@ -119,7 +42,7 @@ export default function BestSeller() {
           viewBox="0 0 20 20"
           xmlns="http://www.w3.org/2000/svg"
         >
-          {bestbook[0].rate}
+          {bestBook[0].rate}
           <title>Second star</title>
           <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
         </svg>
@@ -136,7 +59,7 @@ export default function BestSeller() {
               <a href="#">
                 <img
                   className="object-cover w-full p-5 rounded-t-lg"
-                  src={bestbook[0].cover}
+                  src={bestBook[0].cover}
                   alt="product image"
                 />
               </a>
@@ -147,25 +70,25 @@ export default function BestSeller() {
             <div className="px-5 pb-5">
               <a href="#">
                 <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-                  {bestbook[0].title}
+                  {bestBook[0].title}
                 </h5>
               </a>
               <a href="">
-                <p>{bestbook[0].author}</p>
+                <p>{bestBook[0].author}</p>
               </a>
               <div className="flex justify-center mt-2.5 mb-5">
                 {forloop(5)}
               </div>
               <div>
-                <p>{bestbook[0].description}</p>
+                <p>{bestBook[0].description}</p>
               </div>
             </div>
           </div>
         </div>
 
-        <div className=" grid col-span-3 gap-2 mx-auto text-center">
+        <div className="grid col-span-3 gap-2 mx-auto text-center">
           <div className=" grid grid-cols-4 gap-2 mx-auto">
-            {bestbook.map((book, i) => {
+            {bestBook.map((book, i) => {
               if (i != 0) {
                 return (
                   <div className=" rounded">
--- src/shared/components/Navbar/top-slider/top-slider.jsx
+++ src/shared/components/Navbar/top-slider/top-slider.jsx
@@ -4,7 +4,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-import { Pagination, Navigation } from "swiper";
+import { Navigation } from "swiper";
 
 export default function TopSlider() {
   const SliderData = [
