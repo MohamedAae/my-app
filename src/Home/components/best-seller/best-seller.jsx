da5deea408e0d48dfca639997a9e58e1b1d71637
mawada99 <mawadaahmed973@gmail.com>
2022-07-31 15:04:28 +0200

    Bestsellersection

---
+++ src/Home/components/best-seller/best-seller.jsx
@@ -0,0 +1,187 @@
+import React from "react";
+
+export default function BestSeller() {
+  const bestbook = [
+    {
+      id: 1,
+      price: 125,
+      cover:
+        "https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B300%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&product=path%5B/pimages/9780439064873_p0_v1%5D&call=url%5Bfile:common/decodeProduct.chain%5D",
+      bookurl: "",
+      author: "gg",
+      description: "ftttt",
+      title: "rrr",
+      rate: 5,
+    },
+    {
+      id: 2,
+      price: 125,
+      cover:
+        "https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B300%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&product=path%5B/pimages/9780439064873_p0_v1%5D&call=url%5Bfile:common/decodeProduct.chain%5D",
+      bookurl: "",
+      author: "gg",
+      description: "ftttt",
+      title: "rrr",
+      rate: 3,
+    },
+    {
+      id: 4,
+      price: 125,
+      cover:
+        "https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B300%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&product=path%5B/pimages/9780439064873_p0_v1%5D&call=url%5Bfile:common/decodeProduct.chain%5D",
+      bookurl: "",
+      author: "gg",
+      description: "ftttt",
+      title: "rrr",
+      rate: 4,
+    },
+    {
+      id: 6,
+      price: 125,
+      cover:
+        "https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B300%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&product=path%5B/pimages/9780439064873_p0_v1%5D&call=url%5Bfile:common/decodeProduct.chain%5D",
+      bookurl: "",
+      author: "gg",
+      description: "ftttt",
+      title: "rrr",
+      rate: 4,
+    },
+    {
+      id: 9,
+      price: 125,
+      cover:
+        "https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B300%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&product=path%5B/pimages/9780439064873_p0_v1%5D&call=url%5Bfile:common/decodeProduct.chain%5D",
+      bookurl: "",
+      author: "gg",
+      description: "ftttt",
+      title: "rrr",
+      rate: 4,
+    },
+    {
+      id: 11,
+      price: 125,
+      cover:
+        "https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B300%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&product=path%5B/pimages/9780439064873_p0_v1%5D&call=url%5Bfile:common/decodeProduct.chain%5D",
+      bookurl: "",
+      author: "gg",
+      description: "ftttt",
+      title: "rrr",
+      rate: 4,
+    },
+    {
+      id: 11,
+      price: 125,
+      cover:
+        "https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B300%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&product=path%5B/pimages/9780439064873_p0_v1%5D&call=url%5Bfile:common/decodeProduct.chain%5D",
+      bookurl: "",
+      author: "gg",
+      description: "ftttt",
+      title: "rrr",
+      rate: 4,
+    },
+    {
+      id: 11,
+      price: 125,
+      cover:
+        "https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B300%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&product=path%5B/pimages/9780439064873_p0_v1%5D&call=url%5Bfile:common/decodeProduct.chain%5D",
+      bookurl: "",
+      author: "gg",
+      description: "ftttt",
+      title: "rrr",
+      rate: 4,
+    },
+    {
+      id: 11,
+      price: 125,
+      cover:
+        "https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B300%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&product=path%5B/pimages/9780439064873_p0_v1%5D&call=url%5Bfile:common/decodeProduct.chain%5D",
+      bookurl: "",
+      author: "gg",
+      description: "ftttt",
+      title: "rrr",
+      rate: 4,
+    },
+  ];
+
+  function forloop(n) {
+    let starsarr = [];
+    let color = "text-gray-300";
+    for (var i = 0; i < 5; i++) {
+      if (n > i) {
+        color = "text-yellow-300";
+      }
+      starsarr.push(
+        <svg
+          aria-hidden="true"
+          className={`w-5 h-5 ${color}`}
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          {bestbook[0].rate}
+          <title>Second star</title>
+          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+        </svg>
+      );
+    }
+    return starsarr;
+  }
+  return (
+    <section>
+      <aside className=" grid grid-cols-3  gap-2 mx-auto w-11/12">
+        <div className="grid gap-2">
+          <div className="max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+            <a href="#">
+              <img
+                className="p-8 rounded-t-lg"
+                src={bestbook[0].cover}
+                alt="product image"
+              />
+            </a>
+            <div className="px-5 pb-5">
+              <a href="#">
+                <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                  {bestbook[0].title}
+                </h5>
+              </a>
+              <div className="flex items-center mt-2.5 mb-5">
+                {forloop(5)}
+                <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">
+                  5.0
+                </span>
+              </div>
+              <div className="flex justify-between items-center">
+                <span className="text-3xl font-bold text-gray-900 dark:text-white">
+                  $599
+                </span>
+                <a
+                  href="#"
+                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                  Add to cart
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className=" grid col-span-2 gap-2 mx-auto">
+          <div className=" grid grid-cols-4 gap-2 mx-auto">
+            {bestbook.map((book, i) => {
+              if (i != 0) {
+                return (
+                  <div className=" rounded">
+                    <img
+                      src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
+                      alt="image"
+                    />
+                  </div>
+                );
+              }
+            })}
+          </div>
+        </div>
+      </aside>
+    </section>
+  );
+}
--- src/Home/screen/Home.jsx
+++ src/Home/screen/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import OffersSlider from "../components/offers-slider/offers-slider";
 import TrindingSlider from "../components/trinding-slider/trinding-slider";
+import ImageSection from "../components/image-section";
+import BestSeller from "../components/best-seller/best-seller"
 export default function Home() {
   return (
     <>
       <OffersSlider />
       <TrindingSlider />
+      {/* <ImageSection/> */}
+      <BestSeller/>
     </>
   );
 }
