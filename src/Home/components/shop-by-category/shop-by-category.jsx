51967e56943037ba3dd5e3d66327e1fd8a643380
Wafaa Adel <wafaaadelll99@gmail.com>
2022-08-08 01:52:11 +0200

    Setup category page layout

--- src/Home/components/shop-by-category/shop-by-category.jsx
+++ src/Home/components/shop-by-category/shop-by-category.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { NavLink } from "react-router-dom";
-import { getCategories,getCategoryBooks } from "../../../redux/categories/categories.action";
+import {
+  getCategories,
+  getCategoryBooks,
+} from "../../../redux/categories/categories.action";
 
 let categoryArray = [];
 const ShopByCategory = (props) => {
@@ -10,36 +13,6 @@ const ShopByCategory = (props) => {
     props.getCategoryBooks("62ee7d0a4d3ca74e4a03fcb6");
   }, []);
   categoryArray = props.categories;
-  // let categoryArray = [
-  //   {
-  //     id: 1,
-  //     url: "",
-  //     imageUrl:
-  //       "https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B300%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&set=key%5Bresolve.format%5D,value%5Bjpg%5D&source=url%5Bhttps://prodimage.images-bn.com/pimages/9780593334836_s118x184.jpg%5D&scale=options%5Blimit%5D,size%5B300x10000%5D&sink=format%5Bjpg%5D",
-  //     name: "Manga",
-  //   },
-  //   {
-  //     id: 2,
-  //     url: "",
-  //     imageUrl:
-  //       "https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B300%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&set=key%5Bresolve.format%5D,value%5Bjpg%5D&source=url%5Bhttps://prodimage.images-bn.com/pimages/9780593334836_s118x184.jpg%5D&scale=options%5Blimit%5D,size%5B300x10000%5D&sink=format%5Bjpg%5D",
-  //     name: "Ebooks",
-  //   },
-  //   {
-  //     id: 3,
-  //     url: "",
-  //     imageUrl:
-  //       "https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B300%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&set=key%5Bresolve.format%5D,value%5Bjpg%5D&source=url%5Bhttps://prodimage.images-bn.com/pimages/9780593334836_s118x184.jpg%5D&scale=options%5Blimit%5D,size%5B300x10000%5D&sink=format%5Bjpg%5D",
-  //     name: "Romance",
-  //   },
-  //   {
-  //     id: 4,
-  //     url: "",
-  //     imageUrl:
-  //       "https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B300%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&set=key%5Bresolve.format%5D,value%5Bjpg%5D&source=url%5Bhttps://prodimage.images-bn.com/pimages/9780593334836_s118x184.jpg%5D&scale=options%5Blimit%5D,size%5B300x10000%5D&sink=format%5Bjpg%5D",
-  //     name: "Audiobook",
-  //   },
-  // ];
   return (
     <section className="mb-10 w-11/12 mx-auto">
       <div className="container mx-auto px-2">
@@ -48,14 +21,13 @@ const ShopByCategory = (props) => {
           <div className="grid md:grid-cols-4 w-3/4 mx-auto">
             {categoryArray.map((item, index) => {
               return (
-                <div key={item._id} className="px-1 pb-2 mb-8 border mx-auto w-44 flex items-center justify-center">
+                <div
+                  key={item._id}
+                  className="px-1 pb-2 mb-8 border mx-auto w-44 flex items-center justify-center"
+                >
                   <NavLink to={`/category/${item._id}`}>
                     <h3 className="text-center mb-2 p-1">{item.name}</h3>
-                    <img
-                      className="mx-auto rounded "
-                      src={item.image}
-                      alt=""
-                    />
+                    <img className="mx-auto rounded " src={item.image} alt="" />
                   </NavLink>
                 </div>
               );
@@ -75,7 +47,7 @@ let mapStateToProps = (state) => {
 let mapDispatchToProps = (dispatch) => {
   return {
     getCategories: () => dispatch(getCategories()),
-    getCategoryBooks:(id)=>dispatch(getCategoryBooks(id))
+    getCategoryBooks: (id) => dispatch(getCategoryBooks(id)),
   };
 };
 
--- src/shared/components/categories-books/category-book.jsx
+++ src/shared/components/categories-books/category-book.jsx
@@ -6,49 +6,73 @@ import {
   getCategoryBooks,
 } from "../../../redux/categories/categories.action";
 import { map } from "lodash";
+import { AddToCart } from "../../../redux/cart/cart.action";
+import { useLocation } from "react-router-dom";
+import axios from "axios";
 
 let books = [];
+let categoryname = "";
 const CategoryBook = (props) => {
   const params = useParams();
-
+  console.log(props.books);
   useEffect(() => {
     props.getCategoryBooks(params.id);
+    const getcategoryname = async () => {
+      try {
+        const res = await axios.get(
+          `http://localhost:5003/categories/${params.id}`
+        );
+        categoryname = res.data.category.name;
+        console.log(categoryname);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getcategoryname();
   }, []);
   books = props.books;
-  console.log(props + "boooo");
+
+  const renderHTML = (rawHTML) => {
+    return React.createElement("div", {
+      dangerouslySetInnerHTML: { __html: rawHTML },
+    });
+  };
   return (
     <>
       <h1 className="  px-5 py-6 text-3xl italic font-serif text-center">
-        Category Name
+        {categoryname}
       </h1>
 
       {books.length &&
-        books.map((items) => {
+        books.map((item) => {
           return (
             <section className="w-11/12 mx-auto relative">
               <hr className="absolute w-2/5 top-11 " />
-              <h1 className="  px-5 py-6 text-3xl italic font-serif text-center">
-                {books.length && books[0].name}
+              <h1 className="  px-5 pt-6 text-3xl italic font-serif text-center text-background">
+                {item.name}
               </h1>
               <hr className="absolute w-2/5 top-11 right-0.5 " />
+              <a
+                href="#"
+                className="py-1 italic font-serif text-center w-full block mb-10 text-theme-hover font-black"
+                onClick={() => props.AddToCart(item)}
+              >
+                Buy Now
+              </a>
               <div className="flex w-8/12 mx-auto justify-between">
                 <div className="w-1/4 ">
-                  <img
-                    src="http://prodimage.images-bn.com/pimages/9781953387097_p0_v2_s600x595.jpg"
-                    className="w-11/12"
-                  />
+                  <img src={item.image} className="w-11/12" />
                 </div>
                 <div className="w-3/4  px-3">
-                  <h3 className="italic font-serif">
-                    {books.length && books[0].name}
+                  <h3 className="italic font-serif text-md font-bold">
+                    {item.name}
                   </h3>
-                  <p>author name</p>
+                  <p className="font-semibold text-gray-500 mb-5">
+                    {item.author}
+                  </p>
                   <div>
-                    <p>
-                      Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                      Omnis, quo! Explicabo nisi assumenda animi voluptatem eius
-                      fuga molestiae voluptatum tenetur repudiandae, excepturi
-                      deserunt et dolorem quod vitae repellendus quos quam.
+                    <p className="lowercase">
+                      {renderHTML(item.description.slice(0, 500))}
                     </p>
                   </div>
                 </div>
@@ -69,6 +93,7 @@ let mapStateToProps = (state) => {
 let mapDispatchToProps = (dispatch) => {
   return {
     getCategoryBooks: (id) => dispatch(getCategoryBooks(id)),
+    AddToCart: (book) => dispatch(AddToCart(book)),
   };
 };
 
