1b88c6bc198236558db28db71d71e1eb0bbfaf4c
mawada99 <mawadaahmed973@gmail.com>
2022-08-08 21:37:01 +0200

    Setpage

--- src/redux/products/products.action.js
+++ src/redux/products/products.action.js
@@ -1,52 +1,60 @@
 import axios from "axios";
-import {GETPRODUCTS, GETPRODUCTBYID, GETBESTBOOKS,GETDISCOUNTBOOKS} from "./products.types"
+import {
+  GETPRODUCTS,
+  GETPRODUCTBYID,
+  GETBESTBOOKS,
+  GETDISCOUNTBOOKS,
+} from "./products.types";
 
-export const getProducts = () => async dispatch => {
-    try {
-        const res = await axios.get(`http://127.0.0.1:5003/products`)
-        dispatch({
-            type: GETPRODUCTS,
-            products: res.data.products
-        })
-    } catch (e) {
-        console.log(e.message);
-    }
-}
+export const getProducts = (pageSize, page) => async (dispatch) => {
+  try {
+    const res = await axios.get(
+      `http://127.0.0.1:5003/products?pageSize=${pageSize}&page=${page}`
+    );
+    dispatch({
+      type: GETPRODUCTS,
+      products: res.data.products,
+    });
+  } catch (e) {
+    console.log(e.message);
+  }
+};
 
-export const getBestBooks=()=> async dispatch=>{
-    try {
-        const res = await axios.get(`http://127.0.0.1:5003/products/best-seller`)
-        dispatch({
-            type: GETBESTBOOKS,
-            bestSellerBooks: res.data.bestSellerBooks
-        })
-    } catch (e) {
-        console.log(e.message);
-    }
-}
+export const getBestBooks = () => async (dispatch) => {
+  try {
+    const res = await axios.get(`http://127.0.0.1:5003/products/best-seller`);
+    dispatch({
+      type: GETBESTBOOKS,
+      bestSellerBooks: res.data.bestSellerBooks,
+    });
+  } catch (e) {
+    console.log(e.message);
+  }
+};
 
-export const getProductById = (id) => async dispatch => {
-    try {
-        const res = await axios.get(`http://127.0.0.1:5003/products/${id}`);
-        dispatch({
-            type: GETPRODUCTBYID,
-            product: res.data.product
-        })
-    } catch (e) {
-        console.log(e.message);
-    }
-}
+export const getProductById = (id) => async (dispatch) => {
+  try {
+    const res = await axios.get(`http://127.0.0.1:5003/products/${id}`);
+    dispatch({
+      type: GETPRODUCTBYID,
+      product: res.data.product,
+    });
+  } catch (e) {
+    console.log(e.message);
+  }
+};
 
-
-export const getdiscountbook= (rate) => async dispatch => {
-    try {
-        const res = await axios.get(`http://127.0.0.1:5003/products/discount/${rate}`);
-        dispatch({
-            type: GETDISCOUNTBOOKS,
-            discountbook: res.data.discontbook,
-            rate: rate,
-        })
-    } catch (e) {
-        console.log(e.message);
-    }
-}
+export const getdiscountbook = (rate) => async (dispatch) => {
+  try {
+    const res = await axios.get(
+      `http://127.0.0.1:5003/products/discount/${rate}`
+    );
+    dispatch({
+      type: GETDISCOUNTBOOKS,
+      discountbook: res.data.discontbook,
+      rate: rate,
+    });
+  } catch (e) {
+    console.log(e.message);
+  }
+};
