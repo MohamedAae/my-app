5d4b7a330c40d66c976a4104fde0b5fe840be9ac
Wafaa Adel <wafaaadelll99@gmail.com>
2022-08-09 23:26:30 +0200

    Finalize user account page

--- src/shared/components/user-account/user-account.jsx
+++ src/shared/components/user-account/user-account.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { connect } from "react-redux";
 import axios from "axios";
 import { useEffect } from "react";
+import { Helpers } from "../../helpers";
 
 const API_URL = "http://127.0.0.1:5003";
 let userId;
@@ -24,44 +25,88 @@ const UserAccount = (props) => {
     }
   };
 
-  console.log(reviews);
-
   return (
-    <section className="w-11/12 mx-auto">
-      <h1 className="px-4 py-6 text-3xl italic font-serif">My Account</h1>
-      <div className="flex w-10/12 mx-auto">
-        <div className="flex flex-col w-1/4">
-          <img
-            className="w-1/2 "
-            src="https://flowbite.com/application-ui/demo/images/users/jese-leos-2x.png"
-            alt=""
-          />
-          <div className="py-2 text-serif">
-            <p className="text-gray-500">user name</p>
-            <p>hamada</p>
+    <section className="md:w-11/12 mx-auto">
+      <h1 className="px-4 py-6 text-3xl italic font-serif text-center md:text-left">
+        My Account
+      </h1>
+      <div className="lg:flex lg:w-10/12 w-11/12 mx-auto">
+        <div className="md:flex lg:flex-col lg:w-1/4 md:w-11/12 mx-auto lg:pl-5 pt-5">
+          <div>
+            <img
+              className="lg:w-1/2 md:w-3/4 w-full "
+              src="https://flowbite.com/application-ui/demo/images/users/jese-leos-2x.png"
+              alt=""
+            />
           </div>
-          <div className="py-2 text-serif">
-            <p className="text-gray-500">Email</p>
-            <p>hamada@123.com</p>
-          </div>
-          <div className="py-2 text-serif">
-            <p className="text-gray-500">Phone Number</p>
-            <p>01212121212</p>
+          <div>
+            <div className="py-2 text-serif">
+              <p className="text-gray-500">User Name</p>
+              <p className="capitalize">{props.userName}</p>
+            </div>
+            <div className="py-2 text-serif">
+              <p className="text-gray-500">Email</p>
+              <p>{props.userEmail}</p>
+            </div>
+            <div className="py-2 text-serif">
+              <p className="text-gray-500">Phone Number</p>
+              <p>01212121212</p>
+            </div>
           </div>
         </div>
-        <div className="w-3/4 bg-gray-300">
-          <h1 className="px-4 py-6 text-3xl italic font-serif underline">
+        <div className="lg:w-3/4">
+          <h1 className="lg:px-4 py-6 text-2xl italic font-serif ">
             My Reviews
           </h1>
+          <hr />
           {reviews.length
             ? reviews.map((item) => {
                 return (
-                  <div>
-                    <h3>{item.bookId.name}</h3>
-                    <span>{item.bookId.rating}</span>
-                    <h3>{item.title}</h3>
-                    <p>{item.review}</p>
-                    <p>{new Date(item.date).toLocaleString()}</p>
+                  <div className="bg-gray-100 shadow-lg my-5 rounded-md md:w-11/12 w-full py-5 mx-auto">
+                    <div className="md:w-11/12 mx-auto lg:flex lg:justify-between">
+                      <div className="lg:w-1/5 ">
+                        <img
+                          className="mx-auto"
+                          src={item.bookId.image}
+                          alt=""
+                          width={200}
+                          height={300}
+                        />
+                      </div>
+                      <div className="md:w-4/5 pl:2 md:pl-5 ">
+                        <div>
+                          <h3 className="text-xl font-serif my-5 text-background ">
+                            {item.bookId.name}
+                          </h3>
+                        </div>
+                        <div className="flex">
+                          <span className="font-semibold text-gray-500 mr-2">
+                            My Rate:{" "}
+                          </span>
+                          <span className="flex justify-start items-center">
+                            {Helpers.displayRating(item.bookId.rating)}
+                          </span>
+                        </div>
+                        <div className="flex">
+                          <span className="font-semibold text-gray-500 mr-2">
+                            Title:
+                          </span>
+                          <h3>{item.title}</h3>
+                        </div>{" "}
+                        <div className="md:flex">
+                          <span className="font-semibold text-gray-500 mr-2">
+                            Review:
+                          </span>
+                          <p className="px-2">{item.review.slice(0, 300)}</p>
+                        </div>
+                        <div className="flex">
+                          <span className="font-semibold text-gray-500 mr-2">
+                            Date :
+                          </span>
+                          <p>{new Date(item.date).toLocaleString()}</p>
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 );
               })
@@ -75,6 +120,8 @@ const UserAccount = (props) => {
 const mapStateToProps = (state) => {
   return {
     userId: state.user.user._id,
+    userName: state.user.user.name,
+    userEmail: state.user.user.email,
   };
 };
 
